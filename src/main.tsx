b67a96a4c0eb5bc05c4f574cdb7adc1c38505cec
LordLuffy <2827139+Xenovyrion@users.noreply.github.com>
2026-03-26 08:39:40 +0100

    - Disabling the context menu - Release v1.3.0

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { I18nProvider } from "./i18n";
 import "./index.css";
 
+document.addEventListener("contextmenu", e => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <I18nProvider>
